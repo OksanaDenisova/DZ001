a0d9b4b82df4ae7ecfe9c6cb831f5db71c5a74c6
oks <oksana@gmail.com>
2023-02-11 20:53:39 +0300

    DZ Ended

---
+++ 001/Program.cs
@@ -0,0 +1,71 @@
+﻿// Task1. Напишите программу, которая на вход принимает два числа и выдаёт, 
+//какое число большее, а какое меньшее.
+
+// Console.WriteLine("input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine("First number " + num1 + " greater than the second " + num2);
+// }
+// else 
+// {
+//     Console.WriteLine("Second number " + num2 + " greater than the first " + num1);
+// }
+
+//Task2. Напишите программу, которая принимает на вход три числа и выдаёт 
+//максимальное из этих чисел.
+
+// Console.WriteLine("input a first number: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input a second number: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("input a third number: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num1;
+
+// if (num2 > max)
+//     max = num2;
+// else
+//     num1 = max;
+// if (num3 > max)
+//     max = num3;
+// else 
+//     num3 = max;
+
+// Console.WriteLine("The largest of the three numbers: " + max);
+
+//Task3. Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 1)
+// {
+//     Console.WriteLine("Number " + num + " is odd ");
+// }
+// else
+// {
+//     Console.WriteLine("Number " + num + " is even");
+// }
+
+//Task4.  Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+// Console.Write("input a number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// int current = i;
+
+// while(current <= num)
+// {
+//     if (current % 2 == 0)
+//     {
+//         Console.Write(current + ",");
+//     }
+//     current++;
+// }
