00178657a08895b9d9eba3108602cb7451666b6e
oks <oksana@gmail.com>
2023-02-23 23:51:36 +0300

    004

---
+++ 004/Program.cs
@@ -0,0 +1,63 @@
+﻿/*методы,массивы
+Задача 1. Напишите программу, которая принимает на вход число (А) 
+и выдаёт сумму чисел от 1 до А.*/
+// int GetSum (int a)
+// {
+//     int sum = 0; // переменная куда записываем нащи данные//
+
+//     for(int current = 1; current <= a; current++) //вместо  while записываем цикл таким образом и задаем несколько условий его существования//
+//         sum += current;
+//     return sum;
+//     //пример с while:
+//     // int current = 1;
+//     // while (current<=a);         // их можно использовать в любом случае. то что удобнее то и бери
+//     // {                              //
+//     //     sum += current;
+//     //     current++;
+//     //}
+// }
+// Console.Write("Ввеите число: ");
+// int number  = Convert.ToInt32(Console.ReadLine());
+
+// int result = GetSum(number);
+// Console.WriteLine($"Сумма чисел от 1 до {number} = {result}");
+
+// ЗАДАЧА2.
+//Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+
+// int Digits(int num)
+// {
+//     int digits = 0;
+//     if(num == 0) digits = 1;
+//     while (num != 0)
+//     {
+//         num /= 10;
+//         digits++;
+//     }
+//     return digits;
+// }
+// Console.Write("Enter your number: ");
+// int number  = Convert.ToInt32(Console.ReadLine());
+
+// int dig = Digits(number);
+// Console.WriteLine($"There are {dig} digits {number}");
+
+// ЗАДАЧА 3
+// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+// int Factorial (int num)
+// {
+//     int result = 1;
+//     for(int current = 1; current <= num; current++)
+//     {
+//         result *= current;
+//     }
+//     return result;
+// }
+// Console.Write("Enter your number: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int result =Factorial(number);
+
+// Console.WriteLine($"Factorial of number {number} is {result}");
